a80d91e0e20322c9c0fcbc9c85769d8c6af5e939
fabio.formosa <fabio.formosa@gmail.com>
2018-01-23 00:14:44 +0100

    updated spring boot to ver 1.5.9

--- quartz-manager/src/main/java/it/fabioformosa/ServletInitializer.java
+++ quartz-manager/src/main/java/it/fabioformosa/ServletInitializer.java
@@ -1,7 +1,7 @@
 package it.fabioformosa;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
