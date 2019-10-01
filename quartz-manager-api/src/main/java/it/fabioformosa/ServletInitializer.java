88c740827696b37a8bd263c8842cdd81e1ed1746
fabio.formosa <fabio.formosa@gmail.com>
2019-10-02 00:27:05 +0200

    #4 added javadoc

--- quartz-manager-api/src/main/java/it/fabioformosa/ServletInitializer.java
+++ quartz-manager-api/src/main/java/it/fabioformosa/ServletInitializer.java
@@ -3,6 +3,12 @@ package it.fabioformosa;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * ServletInitializer needs to deploy quartz-manager into a servlet container as a war file
+ *
+ * @author Fabio Formosa
+ *
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
