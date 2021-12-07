3ba2bafc55ef0bfd072af38168169f13666157f9
Fabio Formosa <fabio.formosa@gmail.com>
2021-12-07 23:56:50 +0100

    #55 fixed mockMvcTests

--- quartz-manager-parent/quartz-manager-starter-api/src/test/java/it/fabioformosa/quartzmanager/controllers/utils/TestUtils.java
+++ quartz-manager-parent/quartz-manager-starter-api/src/test/java/it/fabioformosa/quartzmanager/controllers/utils/TestUtils.java
@@ -1,11 +1,17 @@
 package it.fabioformosa.quartzmanager.controllers.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import lombok.SneakyThrows;
 
 public class TestUtils {
 
-   static public ObjectMapper objectMapper = new ObjectMapper();
+  static public ObjectMapper objectMapper = new ObjectMapper();
+  static{
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true)); // StdDateFormat is ISO8601 since jackson 2.9
+  }
 
   @SneakyThrows
   static public String toJson(Object object){
