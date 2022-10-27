0c33eda68c8e4a89b2091afbac21af3092c8850e
Fabio Formosa <fabio.formosa@gmail.com>
2022-10-27 22:58:56 +0200

    added a test for DateUtils

--- quartz-manager-parent/quartz-manager-common/src/main/java/it/fabioformosa/quartzmanager/api/common/utils/DateUtils.java
+++ quartz-manager-parent/quartz-manager-common/src/main/java/it/fabioformosa/quartzmanager/api/common/utils/DateUtils.java
@@ -3,16 +3,21 @@ package it.fabioformosa.quartzmanager.api.common.utils;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class DateUtils {
 
-  static public Date fromLocaleDateTimeToDate(LocalDateTime localDateTime){
-    return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+  static public Date fromLocalDateTimeToDate(LocalDateTime localDateTime){
+    return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant().truncatedTo(ChronoUnit.MILLIS));
   }
 
-  static public Date getHoursFromNow(long hours){
-    return DateUtils.fromLocaleDateTimeToDate(LocalDateTime.now().plus(Duration.ofHours(hours)));
+  static public LocalDateTime fromDateToLocalDateTime(Date date) {
+    return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().truncatedTo(ChronoUnit.MILLIS);
+  }
+
+  static public Date addHoursToNow(long hours){
+    return DateUtils.fromLocalDateTimeToDate(LocalDateTime.now().plus(Duration.ofHours(hours)));
   }
 
 }
