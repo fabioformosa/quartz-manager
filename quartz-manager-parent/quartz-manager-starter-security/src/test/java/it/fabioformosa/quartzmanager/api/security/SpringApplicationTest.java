3af5eb076e3d0367c92296a93b2b93e11a2c77e8
Fabio Formosa <fabio.formosa@gmail.com>
2022-10-29 00:07:15 +0200

    #78 fixed sonar smells

--- quartz-manager-parent/quartz-manager-starter-security/src/test/java/it/fabioformosa/quartzmanager/api/security/SpringApplicationTest.java
+++ quartz-manager-parent/quartz-manager-starter-security/src/test/java/it/fabioformosa/quartzmanager/api/security/SpringApplicationTest.java
@@ -1,19 +1,7 @@
 package it.fabioformosa.quartzmanager.api.security;
 
-import it.fabioformosa.quartzmanager.api.security.controllers.TestController;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApplicationTest {
-
-  @Autowired
-  private TestController testController;
-  @Test
-  public void contextLoad(){
-    Assertions.assertThat(testController).isNotNull();
-  }
-
 }
