5d2b71652c742dc22935f09a0bb085f6607a3a09
Fabio Formosa <fabio.formosa@gmail.com>
2022-11-19 16:25:53 +0100

    #78 fixed 2 sonar warnings

--- quartz-manager-parent/quartz-manager-web-showcase/src/main/java/it/fabioformosa/quartzmanager/jobs/tests/MisfireTestJob.java
+++ quartz-manager-parent/quartz-manager-web-showcase/src/main/java/it/fabioformosa/quartzmanager/jobs/tests/MisfireTestJob.java
@@ -2,6 +2,7 @@ package it.fabioformosa.quartzmanager.jobs.tests;
 
 import it.fabioformosa.quartzmanager.api.jobs.AbstractQuartzManagerJob;
 import it.fabioformosa.quartzmanager.api.jobs.entities.LogRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
  * @author Fabio.Formosa
  *
  */
+@Slf4j
 public class MisfireTestJob extends AbstractQuartzManagerJob {
 
   private Logger log = LoggerFactory.getLogger(MisfireTestJob.class);
@@ -22,11 +24,12 @@ public class MisfireTestJob extends AbstractQuartzManagerJob {
     try {
       log.info("{} is going to sleep...", Thread.currentThread().getName());
 
-      Thread.sleep(10 * 1000);
+      Thread.sleep(10 * 1000L);
 
       log.info("{} woke up!", Thread.currentThread().getName());
     } catch (InterruptedException e) {
-      e.printStackTrace();
+      log.error(e.getMessage(), e);
+      throw new RuntimeException(e);
     }
 
     return new LogRecord(LogRecord.LogType.INFO, "Hello!");
