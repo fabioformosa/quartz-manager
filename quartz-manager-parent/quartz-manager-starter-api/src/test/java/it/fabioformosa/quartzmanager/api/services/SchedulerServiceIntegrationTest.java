0969a406c65e3068ee79ca2e99048856aa3d178e
Fabio Formosa <fabio.formosa@gmail.com>
2022-12-01 00:55:38 +0100

    #62 fixed a failing test

--- quartz-manager-parent/quartz-manager-starter-api/src/test/java/it/fabioformosa/quartzmanager/api/services/SchedulerServiceIntegrationTest.java
+++ quartz-manager-parent/quartz-manager-starter-api/src/test/java/it/fabioformosa/quartzmanager/api/services/SchedulerServiceIntegrationTest.java
@@ -2,12 +2,17 @@ package it.fabioformosa.quartzmanager.api.services;
 
 import it.fabioformosa.quartzmanager.api.dto.SchedulerDTO;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 class SchedulerServiceIntegrationTest {
 
@@ -24,37 +29,29 @@ class SchedulerServiceIntegrationTest {
     Assertions.assertThat(schedulerDTO.getName()).isEqualTo(scheduler.getSchedulerName());
   }
 
+  @Order(1)
   @Test
   void givenASchedulerService_whenTheStatusIsChange_thenTheSchedulerReflectsTheSame() throws SchedulerException {
     Assertions.assertThat(scheduler.isStarted()).isFalse();
     schedulerService.start();
     Assertions.assertThat(scheduler.isStarted()).isTrue();
-
+  }
+  @Order(2)
+  @Test
+  void givenASchedulerService_whenStandByIsCalled_thenTheStandByIsPropagated() throws SchedulerException {
     Assertions.assertThat(scheduler.isInStandbyMode()).isFalse();
     schedulerService.standby();
     Assertions.assertThat(scheduler.isInStandbyMode()).isTrue();
-
-    Assertions.assertThat(scheduler.isShutdown()).isFalse();
-//    schedulerService.shutdown();
-//    Assertions.assertThat(scheduler.isShutdown()).isTrue();
-
   }
 
-//  @Test
-//  void givenASchedulerService_whenStandByIsCalled_thenTheStandByIsPropagated() throws SchedulerException {
-//    Assertions.assertThat(scheduler.isInStandbyMode()).isTrue();
-//    schedulerService.start();
-//    Assertions.assertThat(scheduler.isInStandbyMode()).isFalse();
-//    schedulerService.standby();
-//    Assertions.assertThat(scheduler.isInStandbyMode()).isTrue();
-//  }
-//
-//  @Test
-//  void givenASchedulerService_whenShutdownIsCalled_thenTheShutdownIsPropagated() throws SchedulerException {
-//    Assertions.assertThat(scheduler.isShutdown()).isFalse();
-//    schedulerService.start();
-//    schedulerService.shutdown();
-//    Assertions.assertThat(scheduler.isShutdown()).isTrue();
-//  }
+  @Order(3)
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+  @Test
+  void givenASchedulerService_whenShutdownIsCalled_thenTheShutdownIsPropagated() throws SchedulerException {
+    Assertions.assertThat(scheduler.isShutdown()).isFalse();
+    schedulerService.start();
+    schedulerService.shutdown();
+    Assertions.assertThat(scheduler.isShutdown()).isTrue();
+  }
 
 }
