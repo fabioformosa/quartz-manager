a8330e062f8b90e98f211bd839e8c4807d613258
Fabio Formosa <fabio.formosa@gmail.com>
2022-12-03 19:07:29 +0100

    #62 set jacoco properties

--- quartz-manager-parent/quartz-manager-web-showcase/src/main/java/it/fabioformosa/QuartManagerDemoApplication.java
+++ quartz-manager-parent/quartz-manager-web-showcase/src/main/java/it/fabioformosa/QuartManagerDemoApplication.java
@@ -1,8 +1,10 @@
 package it.fabioformosa;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Generated
 @SpringBootApplication
 public class QuartManagerDemoApplication {
 
