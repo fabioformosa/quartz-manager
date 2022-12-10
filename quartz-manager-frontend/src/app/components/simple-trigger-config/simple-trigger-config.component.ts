109d2868d9141ac7efca01fa7b5f1bd00d71d655
Fabio Formosa <fabio.formosa@gmail.com>
2022-12-10 16:15:00 +0100

    #80 upgraded to the new linter and added prettier

--- quartz-manager-frontend/src/app/components/simple-trigger-config/simple-trigger-config.component.ts
+++ quartz-manager-frontend/src/app/components/simple-trigger-config/simple-trigger-config.component.ts
@@ -3,12 +3,11 @@ import {SchedulerService} from '../../services';
 import {Scheduler} from '../../model/scheduler.model';
 import {SimpleTriggerCommand} from '../../model/simple-trigger.command';
 import {SimpleTrigger} from '../../model/simple-trigger.model';
-import {SimpleTriggerForm} from '../../model/simple-trigger.form';
 import * as moment from 'moment';
 import {TriggerKey} from '../../model/triggerKey.model';
 import JobService from '../../services/job.service';
 import {MisfireInstruction, MisfireInstructionCaption} from '../../model/misfire-instruction.model';
-import {AbstractControl, UntypedFormBuilder, FormControl, UntypedFormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {AbstractControl, UntypedFormBuilder, UntypedFormGroup, ValidationErrors, Validators} from '@angular/forms';
 
 @Component({
   selector: 'qrzmng-simple-trigger-config',
