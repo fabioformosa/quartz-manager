727a11fcea55f34b0f8ac126390a37580bcdd5e2
Fabio Formosa <fabio.formosa@gmail.com>
2022-12-21 00:03:39 +0100

    #103 fixed a test

--- quartz-manager-frontend/src/app/services/logs.rx-websocket.service.spec.ts
+++ quartz-manager-frontend/src/app/services/logs.rx-websocket.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LogsRxWebsocketService } from './logs.rx-websocket.service';
+import {ApiService} from './api.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('LogsRxWebsocketService', () => {
   let service: LogsRxWebsocketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
     service = TestBed.inject(LogsRxWebsocketService);
   });
 
